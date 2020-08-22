756af2688981aacc817ae65d14851e046e88f523
Shah Nawaz <shahnawaz@Shahs-MacBook-Pro.local>
2020-08-22 15:16:26 +0500

    Python sets

---
+++ 01-Python-Object-And-Data-Structure-Basics/08-sets.py
@@ -0,0 +1,11 @@
+# Unordered collection of unique elements.
+my_set = set()
+my_set.add(1)
+print(my_set)  # {1}
+my_set.add(2)
+print(my_set)  # {1,2}
+my_set.add(2)
+print(my_set)  # {1,2}
+
+# Order of alphabets will be random.
+print(set('Mississippi'))
