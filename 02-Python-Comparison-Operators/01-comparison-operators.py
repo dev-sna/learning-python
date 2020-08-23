7bd9a3294c406c1e75d2eb06cdbd8206cc4703de
Shah Nawaz <shahnawaz@Shahs-MacBook-Pro.local>
2020-08-23 21:01:42 +0500

    Python comparison and logical operators

---
+++ 02-Python-Comparison-Operators/01-comparison-operators.py
@@ -0,0 +1,14 @@
+# Equality
+print(2 == 2)  # True
+print('bye' == 'Bye')  # False
+print(2.0 == 2.0)  # True
+
+# Inequality
+3 != 3  # False
+3 != 5  # True
+
+# Less than - Greater than
+print(2 < 3)  # True
+print(2 > 3)  # False
+print(2 >= 2)  # True
+print(3 <= 6)  # True
